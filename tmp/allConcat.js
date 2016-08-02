0acbdd947b4f9e9adb9017d0ce238bae5415e16d
Sebastian Prokuski and Sky Rousse <sebastian@>
2016-08-02 10:40:31 -0700

    draft template for node.js

---
+++ tmp/allConcat.js
@@ -0,0 +1,23 @@
+var Constructor = require('./../js/other-template.js').constructorModule;
+
+
+$(document).ready(function() {
+  $('#generic-form').submit(function(event) {
+    event.preventDefault();
+
+  });
+});
+
+var Constructor = require('./../js/template.js').constructorModule;
+
+
+$(document).ready(function() {
+  $('#generic-form').submit(function(event) {
+    event.preventDefault();
+
+  });
+});
+
+$(document).ready(function(){
+  $('#time').text(moment());
+});

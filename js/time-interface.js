7748df53711c55d479f3de33b73c7a0c1c79771d
Sebastian Adam Prokuski <Subconscious@Sebastians-iMac.local>
2016-08-02 19:43:59 -0700

    fix Clock constructor functionality

--- js/time-interface.js
+++ js/time-interface.js
@@ -1,16 +1,15 @@
 var Clock = require('./../js/time.js').clockModule;
 
 $(document).ready(function(){
-  var myclock = new Clock();
-  setInterval(myclock.update, 1000);
-  $('#alarm-form').submit(function(event) {
-    event.preventDefault();
-    var alarmInput = $('#alarm-input').val();
-    $('#clock').append('<h3>Alarm set for <strong>' + alarmInput + '</strong></h3>');
-    setInterval(function() {
-      myclock.update();
-      myclock.alarmClock(alarmInput);
-    }, 1000);
+  var MyClock = new Clock();
+  setInterval(function() {
+    MyClock.updateTime();
+    $('#radio').text(MyClock.date.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+  }, 1000);
+  $('#alarm-button').click(function() {
+    MyClock.alarm = $('#alarm-input').val();
+    $('#clock').append('<h3>Alarm set for <strong>' + MyClock.alarm + '</strong></h3>');
+    MyClock.alarmClock();
   });
 });
 
--- js/time.js
+++ js/time.js
@@ -1,10 +1,10 @@
-function Clock(datetime, date, time) {
-  this.datetime = null;
+function Clock() {
   this.date = null;
   this.time = null;
+  this.alarm = null;
 }
 
-Clock.prototype.update = function() {
+Clock.prototype.updateTime = function() {
   this.date = moment(new Date());
   dateObject = this.date.toObject();
   if (dateObject.minutes < 10) {
@@ -12,13 +12,12 @@ Clock.prototype.update = function() {
   } else {
     this.time = dateObject.hours + ":" + dateObject.minutes;
   }
-  var datetime = $('#time');
-  datetime.html(this.date.format("dddd, MMMM Do YYYY, h:mm:ss a"));
 };
 
-Clock.prototype.alarmClock = function(alarmInput) {
-  if (this.time === alarmInput) {
+Clock.prototype.alarmClock = function() {
+  if (this.time === this.alarm) {
     document.getElementById('alarm-wav').play();
+    setInterval(this.update, 1000);
   }
 };
 
